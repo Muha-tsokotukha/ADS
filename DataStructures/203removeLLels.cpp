04bbe48931f21aa60c3400fc5dfd763e2257703c
Mukhit <78027029+Muha-tsokotukha@users.noreply.github.com>
2021-09-22 23:31:02 +0600

    day7

---
+++ DataStructures/203removeLLels.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head)return NULL;
+        while(head && head->val == val)head = head->next;
+        if(!head)return NULL;
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+        while( fast ){
+            if(fast->val == val){
+                while(fast && fast->val == val){
+                    fast = fast->next;    
+                }
+                slow->next = fast;
+                
+            }
+            else{
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }
+        return head;
+    }
+};
